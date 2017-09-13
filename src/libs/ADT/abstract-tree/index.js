9d0d2ca18688db08e7a6ceae469a371657d1c6a1
青耕 <qinggeng@gmail.com>
2017-09-14 05:32:46 +0800

    实现广义树过滤函数

--- src/libs/ADT/abstract-tree/index.js
+++ src/libs/ADT/abstract-tree/index.js
@@ -1,37 +1,14 @@
 /**
- * @function filter
- * @brief Tree a -> filter -> filtered Tree b
- * @return: new filtered tree
- * @param node: abstract tree node, will use by rest of params
- * @param predict: function, (node: TreeNode)=>bool, true for keep node, false for abandon
- * @param get_children: (node: TreeNode)=>iterable<TreeNode>, get children of node
- * @param add_children: (node: TreeNode, iterable<TreeNode>)=>undefined, add children to a node
- * @param slice_node: (node: TreeNode)=>TreeNode, slice a node from tree, returns a node WITHOUT parent and any children
+ * @function filter_tree
+ * 广义树过滤器，遍历树节点，检测每个树节点是否符合过滤条件，如果符合，则在结果树中保留这个节点的子树和先祖节点
+ * @param tree 广义树或者树集合
+ * @param funcs 广义树的操作函数
+ * funcs.get_nodes: tree-> array[node] - 将广义树或者广义树集合转化为根节点序列
+ * funcs.predict: node-> boolean - 过滤条件
+ * funcs.slice_subtree: node->node - 复制树节点，同时复制子树
+ * funcs.add_children: (node, children)-> undefiend - 向节点添加子节点，用于为结果树的节点添加过滤的子节点
+ * funcs.build_filtered_tree: (array[node], tree) -> tree - 从过滤好的节点集合构造树或者树集合
  */
-const filter = (node, predict, get_children, add_children, slice_node) =>
-{
-  const children = get_children(node);
-  var new_node = undefined;
-  if (predict(node))
-  {
-    new_node = slice_node(node);
-  }
-  if (undefined === new_node)
-  {
-    return new_node;
-  }
-  const filtered_children = children.reduce((x, y)=>
-  {
-    const node = filter(y);
-    if (undefined !== node)
-    {
-      x.push(node);
-    }
-  }, []);
-  add_children(node, filtered_children);
-  return new_node;
-};
-
 const filter_tree = (tree, funcs) =>
 {
   var filtered_nodes = funcs.get_nodes(tree).map(node=>
@@ -51,54 +28,10 @@ const filter_tree = (tree, funcs) =>
         return filtered_node;
       }
     }
+    return undefined;
   }).filter(x=>x !== undefined);
   return funcs.build_filtered_tree(filtered_nodes, tree);
 }
-
-/**
- * @function arr2tree
- * @brief array -> arr2tree -> tree
- */
-const a2t_bottom_up = (arr, tree_builder) => 
-{
-  var parent_nodes = [];
-  for (var elem of arr)
-  {
-    node = tree_builder.construct_node(elem);
-    var parent = tree_builder.select_parent(node);
-    if (undefined != parent)
-    {
-      parent_nodes.push(parent);
-    }
-  }
-  if (parent_nodes.length == 0)
-  {
-    return arr;
-  }
-  a2t_bottom_up(parent_nodes, tree_builder);
-};
-
-const tree_builder = {
-  construct_node: function(src)
-  {
-  },
-  construct_leaf: function(node)
-  {
-  },
-  construct_branch: function(node)
-  {
-  },
-};
-
-const expect_tree = {
-  'a': {
-    'c': {},
-    'd': {
-      'e': 'foo',
-    },
-  },
-  'b': {},
-};
 /**
  * @function build_prefix_tree
  * 广义前缀树生成器，从广义的前缀数组生成广义的前缀树
@@ -107,7 +40,7 @@ const expect_tree = {
  * @param funcs: 操作函数集，包括对数组，前缀和树的操作，详见函数说明
  * @returns: 生成的广义树
  * @description
- * 这个函数不直接使用前缀和树的任何数据成员，一切对前缀和树的操作都经由func参数传递。funcs参数需要提供如下函数:
+ * 这个函数不直接使用前缀和树的任何数据成员，一切对前缀和树的操作都经由funcs参数传递。funcs参数需要提供如下函数:
  * # 前缀操作
  * ## get_element: (prefix_string)=> prefix_node - 获取前缀串的首个节点
  * ## get_rest: (prefix_string)=> prefix_string - 获取前缀串除首节点以外的剩余部分, 当剩余部分不存在时，应返回undefined
@@ -197,63 +130,104 @@ const build_prefix_tree = (arr, tree, funcs) =>
  *   get_rest,
  * })));
  */
-const folders = `
-src
-src/assets
-src/components
-src/components/bz
-src/components/bz/demo
-src/components/ui
-src/libs
-src/libs/ADT
-src/libs/ADT/abstract-tree
-src/router
-src/store
-`.trim().split('\n').map(x=>x.split('/'))
 
-var select_node = (elem, tree)=>
-{
-  return tree.filter(x=>x.name === elem)[0];
-}
+/**
+ * 树过滤器的例子
+ * const folders = `
+ * src
+ * src/assets
+ * src/components
+ * src/components/bz
+ * src/components/bz/demo
+ * src/components/ui
+ * src/libs
+ * src/libs/ADT
+ * src/libs/ADT/abstract-tree
+ * src/router
+ * src/store
+ * `.trim().split('\n').map(x=>x.split('/'))
+ * 
+ * var select_node = (elem, tree)=>
+ * {
+ *   return tree.filter(x=>x.name === elem)[0];
+ * }
+ * 
+ * var append_node = (elem, tree)=>
+ * {
+ *   var node = {name: elem, children: []};
+ *   tree.push(node);
+ *   return node;
+ * }
+ * 
+ * var get_children = (node)=>
+ * {
+ *   return node.children;
+ * }
+ * 
+ * var get_element = (path)=>
+ * {
+ *   return path[0];
+ * }
+ * 
+ * var get_rest = (path)=>
+ * {
+ *   var ret = path.slice(1);
+ *   if (ret.length === 0)
+ *   {
+ *     return undefined;
+ *   }
+ *   return ret;
+ * }
+ * 
+ * var tree = build_prefix_tree(folders, [], {
+ *   select_node,
+ *   append_node,
+ *   get_children,
+ *   get_element,
+ *   get_rest,
+ * });
+ * 
+ * const funcs = {
+ *   get_nodes: (tree)=>
+ *   {
+ *     return tree;
+ *   },
+ *   predict: (node)=>
+ *   {
+ *     return node.name === 'components';
+ *   },
+ *   slice_subtree: (node)=>
+ *   {
+ *     return JSON.parse(JSON.stringify(node));
+ *   },
+ *   get_children: (node)=>
+ *   {
+ *     return node.children;
+ *   },
+ *   slice_node: (node)=>
+ *   {
+ *     var sliced = JSON.parse(JSON.stringify(node));
+ *     sliced.children = [];
+ *     return sliced;
+ *   },
+ *   add_children: (node, children)=>
+ *   {
+ *     for (var child of children)
+ *     {
+ *       node.children.push(child);
+ *     }
+ *   },
+ *   build_filtered_tree: (filtered, origin)=>
+ *   {
+ *     return filtered;
+ *   },
+ * };
+ * 
+ * const filtered_tree = filter_tree(tree, funcs);
+ * console.log(JSON.stringify(filtered_tree, null, '  '));
+ */
 
-var append_node = (elem, tree)=>
-{
-  var node = {name: elem, children: []};
-  tree.push(node);
-  return node;
-}
-
-var get_children = (node)=>
-{
-  return node.children;
-}
-
-var get_element = (path)=>
-{
-  return path[0];
-}
-
-var get_rest = (path)=>
-{
-  var ret = path.slice(1);
-  if (ret.length === 0)
-  {
-    return undefined;
-  }
-  return ret;
-}
-
-var tree = build_prefix_tree(folders, [], {
-  select_node,
-  append_node,
-  get_children,
-  get_element,
-  get_rest,
-});
-
-/*
 export {
-  filter,
+  filter_tree,
   build_prefix_tree,
 }
-*/
